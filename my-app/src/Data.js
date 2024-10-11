c82fe537279d1dd4ef915a1665a2e874c300f3af
cuongsher <cuongsher@gmail.com>
2024-10-12 02:31:53 +0700

    Thông điệp commit

---
+++ my-app/src/Data.js
@@ -0,0 +1,32 @@
+export const UserData =[
+  {
+    id:1,
+    year:2016,
+    userGain:80000,
+    userLost:823,
+  },
+  {
+    id:2,
+    year:2017,
+    userGain:45677,
+    userLost:443,
+  },
+  {
+    id:3,
+    year:2018,
+    userGain:900,
+    userLost:723,
+  },
+  {
+    id:4,
+    year:2019,
+    userGain:40000,
+    userLost:423,
+  },
+  {
+    id:5,
+    year:2020,
+    userGain:70000,
+    userLost:923,
+  },
+]
